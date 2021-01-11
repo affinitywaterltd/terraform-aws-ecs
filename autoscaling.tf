0b6d7793e57e40e5d77fc46690e2f8273d4e1a96
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-11 10:13:59 +0000

    Test autoscaling

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,9 +1,9 @@
-/*
+
 resource "aws_appautoscaling_target" "ecs_target" {
   count = var.enable_autoscaling ? 1 : 0
   max_capacity       = 4
   min_capacity       = 1
-  resource_id        = "service/clusterName/serviceName"
+  resource_id        = "${var.task_name}-${var.environment_name}/${var.task_name}-${var.environment_name}/${var.task_name}-${var.environment_name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
@@ -12,9 +12,9 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
   count = var.enable_autoscaling ? 1 : 0
   name               = "scale-down"
   policy_type        = "StepScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
@@ -26,4 +26,4 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
       scaling_adjustment          = -1
     }
   }
-}*/
+}
