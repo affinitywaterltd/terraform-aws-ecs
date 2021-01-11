4410524377643e96ecd07d13c9b3d596f76184d6
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-11 10:36:31 +0000

    Change autoscaling

--- autoscaling.tf
+++ autoscaling.tf
@@ -8,22 +8,40 @@ resource "aws_appautoscaling_target" "ecs_target" {
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "ecs_policy" {
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
   count = var.enable_autoscaling ? 1 : 0
-  name               = "scale-down"
-  policy_type        = "StepScaling"
+  name               = "auto-scaling-tracking-cpu"
+  policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
 
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = 60
-    metric_aggregation_type = "Maximum"
-
-    step_adjustment {
-      metric_interval_upper_bound = 0
-      scaling_adjustment          = -1
+  target_tracking_scaling_policy_configuration  {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilistaion"
     }
+
+    target_value = 70
+    scale_in_cooldown = 120
+    scale_out_cooldown = 120
+  }
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+  count = var.enable_autoscaling ? 1 : 0
+  name               = "auto-scaling-tracking-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
+
+  target_tracking_scaling_policy_configuration  {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilistaion"
+    }
+
+    target_value = 70
+    scale_in_cooldown = 120
+    scale_out_cooldown = 120
   }
 }
