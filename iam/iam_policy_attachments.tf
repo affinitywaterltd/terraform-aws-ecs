69a8dcef0cda89af3cd5efa66ec101714af03a7c
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-12 17:32:38 +0000

    Updated count

--- iam/iam_policy_attachments.tf
+++ iam/iam_policy_attachments.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "this_custom" {
-  count = var.create_iam_role && length(var.iam_policies) > 0 ? length(var.iam_policies) : 0
+  count = length(var.iam_policies) > 0 ? length(var.iam_policies) : 0
 
   role       = var.iam_role
   policy_arn = element(var.iam_policies, count.index)
