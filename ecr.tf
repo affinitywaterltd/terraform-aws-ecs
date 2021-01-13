d5dc19ec3d759dae452d39b4ba5918998d0d1581
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-13 09:20:23 +0000

    Updated function

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "this" {
   count = var.create_ecs ? length(var.task_names) : 0
 
-  name                 = lookup(var.task_names, count.index)
+  name                 = element(var.task_names, count.index)
   image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
