2a52b80d894b6bd7736648c3831cf6641ef276d0
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-14 17:27:00 +0000

    Test elimate code

--- ecs_task_definition/ecs_task_definition.tf
+++ ecs_task_definition/ecs_task_definition.tf
@@ -1,18 +1,4 @@
-resource "aws_ecs_task_definition" "this" {
-  family = var.family
-  network_mode = var.network_mode
-  requires_compatibilities = var.requires_compatibilities
-
-  task_role_arn = var.task_role_arn
-  execution_role_arn = var.execution_role_arn
-
-  cpu = var.task_cpu
-  memory = var.task_memory
-  container_definitions = <<EOF
-[
-  {
-    "cpu": ${var.task_cpu},
-    "memory": ${var.task_memory},
+/*
     "dnsSearchDomains": ${var.dnsSearchDomains},
     "environmentFiles": ${var.environmentFiles},
     "secretOptions": ${var.secretOptions},
@@ -41,6 +27,27 @@ resource "aws_ecs_task_definition" "this" {
         "awslogs-stream-prefix": ${var.awslogs-stream-prefix}
       }
     }
+    */
+
+
+
+
+resource "aws_ecs_task_definition" "this" {
+  family = var.family
+  network_mode = var.network_mode
+  requires_compatibilities = var.requires_compatibilities
+
+  task_role_arn = var.task_role_arn
+  execution_role_arn = var.execution_role_arn
+
+  cpu = var.task_cpu
+  memory = var.task_memory
+  container_definitions = <<EOF
+[
+  {
+    "cpu": ${var.task_cpu},
+    "memory": ${var.task_memory},
+
   }
 ]
 EOF
