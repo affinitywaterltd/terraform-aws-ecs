0a3b9da43ea26cb14f048e25daa07f1519d348f6
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-14 16:10:54 +0000

    Update output

--- ecs_task_definition/output.tf
+++ ecs_task_definition/output.tf
@@ -1,4 +1,4 @@
 output "ecs_task_definition_arns" {
   description = "The ARNs of the task definitions"
-  value       = concat(aws_ecs_task_definition.this.*.arn, [""])
+  value       = aws_ecs_task_definition.this.*.arn
 }
