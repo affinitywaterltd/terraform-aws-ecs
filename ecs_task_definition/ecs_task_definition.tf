6c5b3e36c7592ed3794e1558f965623f09891cc2
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-14 17:30:03 +0000

    Test eclusion

--- ecs_task_definition/ecs_task_definition.tf
+++ ecs_task_definition/ecs_task_definition.tf
@@ -1,15 +1,5 @@
 /*
-    "dnsSearchDomains": ${var.dnsSearchDomains},
-    "environmentFiles": ${var.environmentFiles},
-    "secretOptions": ${var.secretOptions},
-    "entryPoint": ${var.entryPoint},
-    "portMappings": ${var.portMappings},
-    "command": ${var.command},
-    "linuxParameters": ${var.linuxParameters},
-    "environment": ${var.environment},
-    "resourceRequirements": ${var.resourceRequirements},
-    "ulimits": ${var.ulimits},
-    "dnsServers": ${var.dnsServers},
+    
     "mountPoints": ${var.mountPoints},
     "workingDirectory": ${var.workingDirectory},
     "secrets": ${var.secrets},
@@ -46,8 +36,18 @@ resource "aws_ecs_task_definition" "this" {
 [
   {
     "cpu": ${var.task_cpu},
-    "memory": ${var.task_memory}
-
+    "memory": ${var.task_memory},
+    "dnsSearchDomains": ${var.dnsSearchDomains},
+    "environmentFiles": ${var.environmentFiles},
+    "secretOptions": ${var.secretOptions},
+    "entryPoint": ${var.entryPoint},
+    "portMappings": ${var.portMappings},
+    "command": ${var.command},
+    "linuxParameters": ${var.linuxParameters},
+    "environment": ${var.environment},
+    "resourceRequirements": ${var.resourceRequirements},
+    "ulimits": ${var.ulimits},
+    "dnsServers": ${var.dnsServers}
   }
 ]
 EOF
