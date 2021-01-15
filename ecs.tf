26827b3cad1a084f2079c4d131dfe012fc364595
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-15 16:08:15 +0000

    Update variable

--- ecs.tf
+++ ecs.tf
@@ -64,7 +64,7 @@ module "aws_ecs_task_definition" {
   stopTimeout = var.stopTimeout
   startTimeout = var.startTimeout
   task_name = "${var.task_names[count.index]}-${var.environment_name}"
-  image_name = length(var.custom_image_names) > 0 ? "${var.custom_image_names[count.index]}:${local.image_tag[count.index]}" : "${aws_ecr_repository.this[count.index].repository_url}:${local.image_tag[count.index]}"
+  image_name = length(var.custom_image_names) > 0 ? "${var.custom_image_names[count.index]}:${local.image_tags[count.index]}" : "${aws_ecr_repository.this[count.index].repository_url}:${local.image_tags[count.index]}"
   logDriver = var.log_driver
   awslogs-region = data.aws_region.current.name
   awslogs-group = "/aws/ecs/cluster/${aws_ecs_cluster.this[0].name}"
