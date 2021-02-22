1148da7e37510b12c2a2e7eb52d7c00b28298eeb
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-22 13:14:14 +0000

    Update LB character length

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,7 +4,7 @@ data "aws_subnet" "this" {
 
 resource "aws_lb_target_group" "this" {
   count = var.create_lb_target_group ? length(var.task_names) : 0
-  name     = "${var.task_names[count.index]}-${var.environment_name}"
+  name     = "${substr(var.task_names[count.index],0,28)}-${var.environment_name}"
   port     = var.container_port
   protocol = var.protocol
   target_type = var.target_type
